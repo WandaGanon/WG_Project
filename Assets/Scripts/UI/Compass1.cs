b22b8aea7a0054fc4250f7df6a6ddca9bf1d9c29
WG <focelotmnighterrorg@gmail.com>
2020-10-04 15:35:52 -0300

    :heavy_plus_sign: Brujula
    se agregaron brujula tradicional y de quest

---
+++ Assets/Scripts/UI/Compass1.cs
@@ -0,0 +1,49 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Compass1 : MonoBehaviour
+{
+    public Transform player;
+    public bool CompassC = false;
+    public bool CompassQ = false;
+    public GameObject Brujula1;
+    public GameObject Brujula2;
+    
+
+    //CompassClassic elementos
+    Vector3 vector;
+    //CompassQuest elementos
+    public RawImage compassImage;
+    void Start()
+    {
+            //Brujula1.active = false;
+            //Brujula2.active = false;
+    }
+    void Update()
+    {
+
+        CompastQuest();
+        
+        //CompastQuest();
+    }
+    public void CompassClassic()
+    {
+        if (CompassQ == false)
+        {
+            CompassC = true;
+        };
+        vector.z = player.eulerAngles.y;
+        transform.localEulerAngles = vector;
+    }
+    public void CompastQuest()
+    {
+        if (CompassC == false)
+        {
+            CompassQ = true;
+        };
+        //uvRect usa los parametros seteados del imageRaw del objeto UI para manejar sus uvs
+        compassImage.uvRect = new Rect(player.localEulerAngles.y / 360f, 0f, 1f, 1f);
+    }
+}
