2a3ea2f55d79daaf6a409675c2aa18e766fc242d
WG <focelotmnighterrorg@gmail.com>
2020-10-05 14:15:39 -0300

    Termino Brujula
    tiene activacion y desactivacion de brujulas, funcionan de manera independiente por lo que sus codigos no chocan

---
+++ Assets/Scripts/UI/Disable.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Disable : MonoBehaviour
+{
+    [Header("Compass Option")]
+    //ELEMENTOS DEL COMPASS CLASSIC
+    [Tooltip("Tecla para activar o Desactivar Compass Classic")]
+    public KeyCode CompassClassicKey = KeyCode.U;
+    public GameObject CompassClassic;
+    //ELEMENTOS DEL COMPASS QUEST
+    [Tooltip("Tecla para activar o Desactivar Compass Quest")]
+    public KeyCode CompassQuestKey = KeyCode.I;
+    public GameObject CompassQuest;
+    //
+    void Start()
+    {
+        CompassClassic.gameObject.SetActive (false);
+        CompassQuest.gameObject.SetActive (false);
+    }
+    void Update()
+    {
+        // ACTIVATE/DESACTIVATE COMPASS CLASSIC
+          if (Input.GetKeyDown(CompassClassicKey) && CompassClassic.activeSelf == false)
+        {
+            CompassClassic.gameObject.SetActive (true);
+            CompassQuest.gameObject.SetActive (false);
+        }
+        else if (Input.GetKeyDown(CompassClassicKey) && CompassClassic.activeSelf == true)
+        {
+            CompassClassic.gameObject.SetActive (false);
+        }
+        // ACTIVATE/DESACTIVATE COMPASS QUEST
+        if (Input.GetKeyDown(CompassQuestKey) && CompassQuest.activeSelf == false)
+        {
+            CompassQuest.gameObject.SetActive (true);
+            CompassClassic.gameObject.SetActive (false);
+        }
+        else if (Input.GetKeyDown(CompassQuestKey) && CompassQuest.activeSelf == true)
+        {
+            CompassQuest.gameObject.SetActive (false);
+        }
+    }
+}
