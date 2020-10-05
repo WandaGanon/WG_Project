477fd26ffa1cbf07654e6bb12c3d68672d34ad47
WG <focelotmnighterrorg@gmail.com>
2020-10-05 19:19:35 -0300

    :hammer: creado pausa, exit y realtimer
    se agregaron 3 codigos al script del menu

---
+++ Assets/Scripts/Menu/Menu.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using System;
+
+public class Menu : MonoBehaviour
+{
+    public Text TimeText;
+    public string TimeString;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+       Timer();
+    }
+    
+    public void Timer()
+    {
+        DateTime now = DateTime.Now;
+        TimeString = DateTime.Now.ToString("hh:mm:ss");
+        TimeText.text = TimeString;
+    }
+    public void Pause ()
+    {
+        if (Time.timeScale == 1)
+        {
+        Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+    }
+    public void ExitGame()
+    {
+        UnityEngine.Debug.LogError("Exit Game");
+        Application.Quit(); 
+    }
+}
