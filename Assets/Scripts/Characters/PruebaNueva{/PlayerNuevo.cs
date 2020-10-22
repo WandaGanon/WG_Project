62ec584aecf4973e8a99137e2fe95efd0bc924f7
Vulgrim <newana7168@qatw.net>
2020-10-22 20:21:54 -0300

    :ambulance: Arreglo de controler y funcional
    Se arregla problema del controlador

---
+++ Assets/Scripts/Characters/PruebaNueva{/PlayerNuevo.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerNuevo : MonoBehaviour
+{
+// Informacion Obtenida de https://www.youtube.com/watch?v=FvvTDkJvBfA&list=PLOc9rHNEUHlyryuY0PvipHTXyL2mBij9-
+//https://www.youtube.com/watch?v=Dp_MFvT_VIw&list=PLOc9rHNEUHlyryuY0PvipHTXyL2mBij9-&index=2&ab_channel=GamerGarage
+    public float h;
+    public float v;
+    private Vector3 playerInput;
+    private Vector3 moverPlayer;
+    public CharacterController Player;
+    public float Velocidad = 0.1f;
+
+    public Camera mainCamera;
+    private Vector3 camForward;
+    private Vector3 camRight;
+
+    void Start()
+    {
+        Player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        h = Input.GetAxis("Horizontal");
+        v = Input.GetAxis("Vertical"); 
+
+        playerInput = new Vector3(h,0, v);
+
+        //Es te es un ajuste para que cuando se realiza un velocidad en diagonal no sea superior al maximo
+        playerInput = Vector3.ClampMagnitude(playerInput, 1);
+
+        camDireccion();
+        moverPlayer = playerInput.x * camRight + playerInput.z * camForward;
+
+        Player.transform.LookAt(Player.transform.position + moverPlayer);
+
+        Player.Move( moverPlayer * Velocidad * Time.deltaTime);
+        
+    }
+
+    void camDireccion(){
+
+        camForward = mainCamera.transform.forward;
+        camRight = mainCamera.transform.right;
+
+        camForward.y =  0;
+        camRight.y = 0;
+        
+        camForward = camForward.normalized;
+        camRight = camRight.normalized;
+
+    }
+    private void FixedUpdate() {
+    }
+}
