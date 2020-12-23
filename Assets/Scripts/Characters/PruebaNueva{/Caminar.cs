9dcde535e28ef332c3d7c321a4d4f254917b1ee0
Vulgrim <newana7168@qatw.net>
2020-12-22 23:56:03 -0300

    🚧  Se corrige el problema del salto

---
+++ Assets/Scripts/Characters/PruebaNueva{/Caminar.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Caminar : MonoBehaviour
+{
+    public float Horizontal;
+    public float Vertical;
+    private Animator anim;
+
+    void Start(){
+
+    anim = GetComponent<Animator>();
+
+    }
+
+    void Update()
+    {
+        Horizontal = Input.GetAxis("Horizontal");
+        Vertical = Input.GetAxis("Vertical");
+        Idle(); 
+
+    }
+
+    public void  Idle()
+    {
+        if(Horizontal == 0  && Horizontal == 0){ 
+            anim.SetBool("waitIdle?", true); 
+        }
+        else{ 
+            anim.SetBool("waitIdle?", false);
+        }
+    }  
+
+}
